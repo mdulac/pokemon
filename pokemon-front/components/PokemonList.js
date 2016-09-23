9da95a2a48e457997fbc9c2dd987e2d234cf528d
mdulac <mat.dulac@gmail.com>
2016-09-23 22:42:08 +0200

    Add Avatar and Button in list

--- pokemon-front/components/PokemonList.js
+++ pokemon-front/components/PokemonList.js
@@ -3,14 +3,28 @@ import Table from "material-ui/lib/table/table";
 import TableRow from "material-ui/lib/table/table-row";
 import TableRowColumn from "material-ui/lib/table/table-row-column";
 import TableBody from "material-ui/lib/table/table-body";
+import Avatar from 'material-ui/lib/Avatar';
+import FlatButton from 'material-ui/lib/flat-button';
+
+const _ = require('lodash');
 
 const PokemonList = ({pokemons}) => {
+
+    const style = {margin: 5};
+
     return (
         <Table selectable={false}>
             <TableBody showRowHover={true} displayRowCheckbox={false}>
                 {pokemons.map(pokemon =>
                     <TableRow key={pokemon.name}>
-                        <TableRowColumn>{pokemon.name}</TableRowColumn>
+                        <TableRowColumn>
+                            <Avatar
+                                size={30}
+                                style={style}>
+                                {_.toUpper(pokemon.name[0])}
+                            </Avatar>
+                            <FlatButton label={pokemon.name} />
+                        </TableRowColumn>
                     </TableRow>
                 )}
             </TableBody>
