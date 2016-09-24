203fab84d821a5028b51010307abf5d28cc6d46e
mdulac <mat.dulac@gmail.com>
2016-09-24 16:05:12 +0200

    Fix props declaration and display tabs

--- pokemon-front/components/PokemonDetails.js
+++ pokemon-front/components/PokemonDetails.js
@@ -2,32 +2,50 @@ import React, {PropTypes} from "react";
 import Avatar from "material-ui/lib/Avatar";
 import List from "material-ui/lib/lists/list";
 import ListItem from "material-ui/lib/lists/list-item";
+import Tabs from "material-ui/lib/tabs/tabs";
+import Tab from "material-ui/lib/tabs/tab";
 
+const injectTapEventPlugin = require("react-tap-event-plugin");
 const _ = require('lodash');
+
+injectTapEventPlugin();
 
 const PokemonDetails = ({pokemon}) => {
     return (
-        <List>
-            <ListItem>
-                <Avatar backgroundColor="white"
-                        size={100}
-                        src={pokemon.sprite}
-                />
-            </ListItem>
-            <ListItem primaryText={`Id : ${pokemon.id ? pokemon.id : ''}`}/>
-            <ListItem primaryText={`Name : ${pokemon.name ? pokemon.name : ''}`}/>
-            <ListItem primaryText={`Height : ${pokemon.height ? pokemon.height : ''}`}/>
-            <ListItem primaryText={`Weight : ${pokemon.weight ? pokemon.weight : ''}`}/>
-            {pokemon.stats ? (<ListItem>
-                <List>
-                    {pokemon.stats.map(s => {
-                        return (
-                            <ListItem primaryText={`Name : ${s.name} | Effort : ${s.effort} | Base stat : ${s.base_stat}`}/>
-                        )
-                    })}
-                </List>
-            </ListItem>) : null}
-        </List>
+        <Tabs>
+            <Tab label="General">
+                <div>
+                    <List>
+                        <ListItem>
+                            <Avatar backgroundColor="white"
+                                    size={100}
+                                    src={pokemon.sprite}
+                            />
+                        </ListItem>
+                        <ListItem primaryText={`Id : ${pokemon.id ? pokemon.id : ''}`}/>
+                        <ListItem primaryText={`Name : ${pokemon.name ? pokemon.name : ''}`}/>
+                        <ListItem primaryText={`Height : ${pokemon.height ? pokemon.height : ''}`}/>
+                        <ListItem primaryText={`Weight : ${pokemon.weight ? pokemon.weight : ''}`}/>
+                    </List>
+                </div>
+            </Tab>
+            <Tab label="Stats">
+                <div>
+                    <List>
+                        {pokemon.stats ? (<ListItem>
+                            <List>
+                                {pokemon.stats.map(s => {
+                                    return (
+                                        <ListItem key={s.name}
+                                                  primaryText={`Name : ${s.name} | Effort : ${s.effort} | Base stat : ${s.base_stat}`}/>
+                                    )
+                                })}
+                            </List>
+                        </ListItem>) : null}
+                    </List>
+                </div>
+            </Tab>
+        </Tabs>
     )
 };
 
@@ -38,11 +56,12 @@ PokemonDetails.propTypes = {
         height: PropTypes.number.isRequired,
         weight: PropTypes.number.isRequired,
         sprite: PropTypes.string.isRequired,
-        stats: PropTypes.shape({
-            name: PropTypes.string.isRequired,
-            effort: PropTypes.number.isRequired,
-            base_stat: PropTypes.number.isRequired
-        }).isRequired
+        stats: PropTypes.arrayOf(
+            PropTypes.shape({
+                name: PropTypes.string.isRequired,
+                effort: PropTypes.number.isRequired,
+                base_stat: PropTypes.number.isRequired
+            })).isRequired
     }).isRequired
 };
 
