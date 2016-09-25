56d1f1a7d2ea25ac4b95551490c51ab6add8331f
mdulac <mat.dulac@gmail.com>
2016-09-25 19:36:19 +0200

    fetchStats and statsAverage

--- src/main/scala/controllers/PokemonController.scala
+++ src/main/scala/controllers/PokemonController.scala
@@ -10,7 +10,8 @@ import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
 import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.stream.Materializer
 import model.RequestFailure.{InvalidName, NameNotFound}
-import model.{PokemonCount, PokemonDetails, RequestFailure}
+import model._
+import spray.json._
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -23,6 +24,23 @@ class PokemonController(val logger: LoggingAdapter,
     pokemonApiRequest(RequestBuilding.Get(s"/api/v2/pokemon/$name/")).flatMap { response =>
       response.status match {
         case OK => Unmarshal(response.entity).to[PokemonDetails].map(Right(_))
+        case BadRequest => Future.successful(Left(InvalidName(name)))
+        case NotFound => Future.successful(Left(NameNotFound(name)))
+        case _ =>
+          Unmarshal(response.entity).to[String].flatMap { entity =>
+            val error = s"Pokeapi request failed with status code ${response.status} and entity $entity"
+            logger.error(error)
+            Future.failed(new IOException(error))
+          }
+      }
+    }
+  }
+
+  val fetchStats: Process[String, Either[RequestFailure, PokemonStats]] = name => {
+    logger.debug(s"Fetching Pokemon $name stats")
+    pokemonApiRequest(RequestBuilding.Get(s"/api/v2/pokemon/$name/")).flatMap { response =>
+      response.status match {
+        case OK => Unmarshal(response.entity).to[PokemonStats].map(Right(_))
         case BadRequest => Future.successful(Left(InvalidName(name)))
         case NotFound => Future.successful(Left(NameNotFound(name)))
         case _ =>
@@ -83,11 +101,18 @@ class PokemonController(val logger: LoggingAdapter,
     }
   }
 
-  val stats: Process[String, String] = name => {
+  val statsAverage: Process[String, List[(String, Float)]] = name => {
     logger.debug(s"Fetching Pokemon $name stats")
     fetch(name).flatMap {
       case Right(pokemon) =>
-        Future.sequence(pokemon.types.map(t => fetchType(t.name))).map(_.mkString("[", ",", "]"))
+        for {
+          types <- Future.sequence(pokemon.types.map(t => fetchType(t.name)))
+          pokemonNames <- Future.successful(types.head.parseJson.convertTo[List[PokemonName]])
+          results <- Future.sequence(pokemonNames.map(pokemonName => fetchStats(pokemonName.name)))
+          stats <- Future.successful(results.collect {
+            case Right(stat) => stat
+          }.flatMap(_.stats))
+        } yield Stat.averageOf(stats)
       case Left(cause) =>
         Future.failed(new IOException(cause.message))
     }
